f371565ef4a2c3ba54bda812b20f9b6dffc7faf9
whiteRa2bit <pavel.fakanov@gmail.com>
2020-10-31 11:34:05 +0000

    Update run_train

--- tools/run_train.py
+++ tools/run_train.py
@@ -2,7 +2,7 @@ import torch
 
 from nmt.vocab import get_src_vocab, get_trg_vocab
 from nmt.dataset import TextDataset
-from nmt.nets.baseline_net import Encoder, Decoder
+from nmt.nets.baseline_net import BasicNet
 from nmt.trainer import Trainer
 from nmt.config import TRAIN_SRC_PATH, TRAIN_TRG_PATH, VAL_SRC_PATH, VAL_TRG_PATH, CONFIG
 
@@ -16,12 +16,10 @@ def main():
     train_dataset = TextDataset(TRAIN_SRC_PATH, TRAIN_TRG_PATH, src_vocab, trg_vocab)
     val_dataset = TextDataset(VAL_SRC_PATH, VAL_TRG_PATH, src_vocab, trg_vocab)
 
-    encoder = Encoder(src_vocab, train_config)
-    decoder = Decoder(trg_vocab, train_config)
-    params = list(encoder.parameters()) + list(decoder.parameters())
-    optimizer = torch.optim.Adam(params, lr=train_config["lr"])
+    model = BasicNet(src_vocab, train_config)
+    optimizer = torch.optim.Adam(model.parameters(), lr=train_config["lr"])
 
-    trainer = Trainer(encoder, decoder, optimizer, train_dataset, val_dataset, train_config)
+    trainer = Trainer(model, optimizer, train_dataset, val_dataset, train_config)
     trainer.train()
 
 
